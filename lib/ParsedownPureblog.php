94b0dd74a6807a2d85e57b6f92accf87182fa8e4
Kev Quirk <kev@quirk.cc>
2026-07-12 11:55:15 +0100

    Implementing #35

--- lib/ParsedownPureblog.php
+++ lib/ParsedownPureblog.php
@@ -27,6 +27,94 @@ class ParsedownPureblog extends ParsedownExtra
             }
         }
 
+        // Add rel="noopener noreferrer" to external markdown links
+        if (
+            isset($Element['name']) && $Element['name'] === 'a'
+            && isset($Element['attributes']['href'])
+        ) {
+            $href = $Element['attributes']['href'];
+            // Check if it's an external link (starts with http:// or https:// or //)
+            if (preg_match('/^(https?:)?\/\//i', $href)) {
+                $isExternal = true;
+                if (function_exists('load_config')) {
+                    $config = load_config();
+                    $baseUrl = trim($config['base_url'] ?? '');
+                    if ($baseUrl !== '') {
+                        $host = parse_url($href, PHP_URL_HOST);
+                        $baseHost = parse_url($baseUrl, PHP_URL_HOST);
+                        if ($host && $baseHost && strcasecmp($host, $baseHost) === 0) {
+                            $isExternal = false;
+                        }
+                    }
+                }
+
+                if ($isExternal) {
+                    if (!isset($Element['attributes']['rel'])) {
+                        $Element['attributes']['rel'] = 'noopener noreferrer';
+                    }
+                }
+            }
+        }
+
         return parent::element($Element);
+    }
+
+    protected function inlineLink($Excerpt)
+    {
+        $Link = parent::inlineLink($Excerpt);
+        if ($Link === null) {
+            return null;
+        }
+
+        $remainder = substr($Excerpt['text'], $Link['extent']);
+
+        // Matches curly brace attribute block (e.g. {target="_blank" class="custom"})
+        if (preg_match('/^[ ]*{(.*?)}/', $remainder, $matches)) {
+            $attributeData = $this->parseCustomAttributes($matches[1]);
+            if (!empty($attributeData)) {
+                if (isset($attributeData['class']) && isset($Link['element']['attributes']['class'])) {
+                    $attributeData['class'] = $Link['element']['attributes']['class'] . ' ' . $attributeData['class'];
+                }
+                $Link['element']['attributes'] = array_merge($Link['element']['attributes'], $attributeData);
+                $Link['extent'] += strlen($matches[0]);
+            }
+        }
+
+        return $Link;
+    }
+
+    protected function parseCustomAttributes(string $attributeString): array
+    {
+        $Data = array();
+        $classes = array();
+
+        // Match .class, #id, or key="value" / key='value' / key=value
+        $pattern = '/\.([-\w]+)|#([-\w]+)|([-\w]+)\s*=\s*(?:"([^"]*)"|\'([^\']*)\'|([^\s}]+))/';
+        if (preg_match_all($pattern, $attributeString, $matches, PREG_SET_ORDER)) {
+            foreach ($matches as $match) {
+                if (!empty($match[1])) {
+                    $classes[] = $match[1];
+                } elseif (!empty($match[2])) {
+                    $Data['id'] = $match[2];
+                } elseif (!empty($match[3])) {
+                    $key = $match[3];
+                    $val = '';
+                    if (isset($match[4]) && $match[4] !== '') {
+                        $val = $match[4];
+                    } elseif (isset($match[5]) && $match[5] !== '') {
+                        $val = $match[5];
+                    } elseif (isset($match[6]) && $match[6] !== '') {
+                        $val = $match[6];
+                    }
+                    $Data[$key] = $val;
+                }
+            }
+        }
+
+        if (!empty($classes)) {
+            $Data['class'] = implode(' ', $classes);
+        }
+
+        return $Data;
     }
 }
